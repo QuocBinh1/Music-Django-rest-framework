56d4b712bcfce3b265368673b9d722a52c0b78bc
QuocBinh1 <BinhPlayll@gmail.com>
2025-03-16 20:51:35 +0700

    first commit

--- user/models.py
+++ user/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
+    nd_manguoidung = models.CharField(max_length=11)
+    nd_email = models.EmailField()
+    nd_matkhau = models.CharField(max_length=50)
+    nd_sodienthoai = models.CharField(max_length=20)
+
 
     def __str__(self):
         return self.name
